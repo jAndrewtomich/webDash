6cc368c313e3f63a9a50f356794f78955ae7cbc1
jatomic <johnatomich@gmail.com>
2022-06-08 21:53:48 -0500

    File Structure

--- src/app1/models.py
+++ src/app1/models.py
@@ -4,3 +4,11 @@ from django.db import models
 class CSVFile(models.Model):
     name = models.CharField(max_length=100)
     csvFile = models.FileField(upload_to='app1/csvs')
+
+    def __str__(self):
+        return self.name
+
+
+# class Participant(models.Model):
+#     name = models.CharField(max_length=10)
+#     role = models.CharField(max_length=50)
--- src/app1/views.py
+++ src/app1/views.py
@@ -11,7 +11,39 @@ from rest_framework import authentication, permissions
 from app1.models import CSVFile
 
 import pandas as pd
+import re, random
 
+
+
+###### READ IN CSVs ##############
+df = (tmp := pd.read_csv(CSVFile.objects.all()[0].csvFile.name,
+                        usecols=['Text',
+                                'Tag',
+                                'Tag - Group',
+                                'Note - Role',
+                                'Note - Industry',
+                                'Note - Participant ID'])
+     ).drop(
+    tmp.loc[tmp['Note - Participant ID'].str.contains('MKT'), :].index).rename(
+    {'Text': 'Evidence',
+     'Tag - Group': 'Group',
+     'Note - Role': 'Role',
+     'Note - Industry': 'Industry',
+     'Note - Participant ID': 'Participant'}, axis=1).dropna()
+
+orgdf = (tmp := pd.read_csv(CSVFile.objects.all()[1].csvFile.name)).drop(tmp.loc[tmp['Participant ID'].str.contains('P4'), :].index)
+
+parDict = {int(x.split('P')[-1]): f"P{int(x.split('P')[-1])}" for x in df.Participant.unique()}
+
+df['Participant'] = df['Participant'].map(lambda x: {v: k for k, v in parDict.items()}[f"P{int(x.split('P')[-1])}"])
+orgdf['Participant ID'] = orgdf['Participant ID'].map(lambda x: {v: k for k, v in parDict.items()}[f"P{int(x.split('P')[-1])}"])
+
+df['orgSize'] = df['Participant'].map(lambda x: orgdf[orgdf['Participant ID'] == x]['Org Size'].item()
+                                      .replace(' to ', '-').replace(' +', '-99,999').replace('1000-4999', '1,000-4,999'))
+
+orgDict = {int(re.split(r'\D', x.replace(',', ''))[0]): x for x in df.orgSize}
+del df, orgdf
+####################################
 
 
 def environment(**options):
@@ -31,7 +63,18 @@ class HomeView(TemplateView):
 
     def get(self, request, *args, **kwargs):
 
-        return render(request, 'app1/index.html', {})
+        file_name = CSVFile.objects.all()[2].csvFile.name
+
+        df = pd.read_csv('{}'.format(file_name))
+
+
+        return render(request, 'app1/index.html', {'data': {
+            'columns': {
+                x: df[x].to_list() for x in df.columns
+            },
+            'parDict': parDict,
+            'orgDict': orgDict
+        }})
 
 
 
@@ -74,20 +117,19 @@ class ChartData(APIView):
 
     def get(self, request, format=None):
 
-        df = pd.read_csv(CSVFile.objects.all()[0].csvFile)
+        df = pd.read_csv(CSVFile.objects.all()[2].csvFile).astype('category')
 
-        x = df.Participant.to_list()
-        y = df.orgSize.to_list()
-        z = df.Cadence.to_list()
+        xValues = [p + random.uniform(-0.05, 0.05) for p in df.orgSize.cat.codes.to_list()]
+        yValues = [i + random.uniform(-0.05, 0.05) for i in df.Cadence.cat.codes.to_list()]
+        labels = df.Participant.to_list()
 
         data = {
             'coords': [
-                {'x': a,
-                 'y': b,
-                    # 'z': c,
-                }
-                    for (a, b) in zip(x, y)
+                {'x': x,
+                 'y': y
+                } for x, y in zip(xValues, yValues)
             ],
-            'labels': [str(s) for s in z],
+            'labels': labels
         }
+
         return Response(data)
